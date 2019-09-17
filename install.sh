827df7540e88aa5426be8b7fc5b4a822fef179ba
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-09-17 14:01:07 -0400

    Updated the build layout and where we get some information from.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-build_path=$1
-install_path=$2
-vscode_version=$3
+EXTRACT_PATH=$1
+INSTALL_PATH=${REZ_BUILD_INSTALL_PATH}
+VSCODE_VERSION=${REZ_BUILD_PROJECT_VERSION}
 
 # We print the arguments passed to the Bash script
 echo -e "\n"
@@ -11,17 +11,17 @@ echo -e "=== INSTALL ==="
 echo -e "==============="
 echo -e "\n"
 
-echo -e "[INSTALL][ARGS] BUILD PATH: ${build_path}"
-echo -e "[INSTALL][ARGS] VSCODE VERSION: ${vscode_version}"
+echo -e "[INSTALL][ARGS] BUILD PATH: ${EXTRACT_PATH}"
+echo -e "[INSTALL][ARGS] VSCODE VERSION: ${VSCODE_VERSION}"
 
-cd $build_path
+cd ${EXTRACT_PATH}
 
 # We finally install VSCode
 echo -e "\n"
-echo -e "[INSTALL] Installing VSCode-${vscode_version}..."
+echo -e "[INSTALL] Installing VSCode-${VSCODE_VERSION}..."
 
 # We copy the necessary files to the install directory
-cp -R ./* $install_path
+cp -R ./* ${INSTALL_PATH}
 
-echo -e "[INSTALL] Finished installing VSCode-${vscode_version}!"
+echo -e "[INSTALL] Finished installing VSCode-${VSCODE_VERSION}!"
 echo -e "\n"
