b699228059b2b4c63bb5b522624be41559a5a581
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-09-19 19:41:25 -0400

    Misc updates in the Bash script(s).

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ EXTRACT_PATH=$1
 INSTALL_PATH=${REZ_BUILD_INSTALL_PATH}
 VSCODE_VERSION=${REZ_BUILD_PROJECT_VERSION}
 
-# We print the arguments passed to the Bash script
+# We print the arguments passed to the Bash script.
 echo -e "\n"
 echo -e "==============="
 echo -e "=== INSTALL ==="
@@ -18,14 +18,12 @@ echo -e "[INSTALL][ARGS] EXTRACT PATH: ${EXTRACT_PATH}"
 echo -e "[INSTALL][ARGS] INSTALL PATH: ${INSTALL_PATH}"
 echo -e "[INSTALL][ARGS] VSCODE VERSION: ${VSCODE_VERSION}"
 
-cd ${EXTRACT_PATH}
-
-# We install VSCode
+# We install VSCode.
 echo -e "\n"
 echo -e "[INSTALL] Installing VSCode-${VSCODE_VERSION}..."
 
-# We copy the necessary files to the install directory
-cp -R ./* ${INSTALL_PATH}
+# We copy the necessary files to the install directory.
+cp -R ${EXTRACT_PATH}/* ${INSTALL_PATH}
 
 echo -e "[INSTALL] Finished installing VSCode-${VSCODE_VERSION}!"
 echo -e "\n"
